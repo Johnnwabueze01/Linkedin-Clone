1ec521cce2188d884d492593ccba57caa74edf1d
Nwabueze John <143359560+Johnnwabueze01@users.noreply.github.com>
2024-09-23 16:59:47 +0100

    Update Header.jsx

--- src/Header.jsx
+++ src/Header.jsx
@@ -4,18 +4,7 @@ import App from './App';
 import Body from './Body';
 import pagelogo from './assets/page-logo..jpg'
 
-// const Header = () => {
-  /*  function Header() {
-    return (
-        <header style={headerStyle}>
-            <h1>LinkedIn Clone</h1>
-            <nav>
-                <a href="#login">Login</a>
-                <a href="#signup">Sign Up</a>
-            </nav>
-        </header>
-    )
-};*/
+
 function Header(){
     return (
         <header>
@@ -24,7 +13,6 @@ function Header(){
             <img src={pagelogo} alt="linkedin logo" />
          </div>
         <nav>
-            <a href="page.jsx">join now</a>
             <button href="Page.jsx" className="btn_l">Login</button>
             <button href="#signup" className="btn_s">Sign Up</button>
         </nav>
